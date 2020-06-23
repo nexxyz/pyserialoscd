29ec3a42b2be95997ba67673bd1b68dd649f99df
Thomas Steirer <nexxyz@gmail.com>
2020-06-23 18:01:55 +0200

    fix indent

--- pyserialosctestharness.py
+++ pyserialosctestharness.py
@@ -9,39 +9,39 @@ import pyserialoscutils
 
 
 def keyboardInterruptHandler(signal, frame):
-    print("KeyboardInterrupt (ID: {}) has been caught. Cleaning up...".format(signal))
-    print("Stopping testapp")
-    testapplication.stop()
-    exit(0)
+        print("KeyboardInterrupt (ID: {}) has been caught. Cleaning up...".format(signal))
+        print("Stopping testapp")
+        testapplication.stop()
+        exit(0)
 
 
 # -----------
 # Main entry point
 # -----------
 if __name__ == "__main__":
-  signal.signal(signal.SIGINT, keyboardInterruptHandler)
+    signal.signal(signal.SIGINT, keyboardInterruptHandler)
 
-  testapplication_ip = "localhost"
-  testapplication_port = 15555
-  serialosc_ip = "localhost"
-  serialosc_port = 12002
-  device_ip = "localhost"
-  device_port = 12235
+    testapplication_ip = "localhost"
+    testapplication_port = 15555
+    serialosc_ip = "localhost"
+    serialosc_port = 12002
+    device_ip = "localhost"
+    device_port = 12235
 
-  # Test Application
-  testapplication = pyserialoscutils.OscServerWrapper("testapplication")
-  testapplication.start(testapplication_ip, testapplication_port)
+    # Test Application
+    testapplication = pyserialoscutils.OscServerWrapper("testapplication")
+    testapplication.start(testapplication_ip, testapplication_port)
 
-  print("Press CTRL-C to stop")
-  pyserialoscutils.OscClientWrapper(serialosc_ip, serialosc_port).send_message(
-      "/serialosc/list", "localhost", 15555)
-  pyserialoscutils.OscClientWrapper(
-      device_ip, device_port).send_message("/info", "localhost", 15555)
+    print("Press CTRL-C to stop")
+    pyserialoscutils.OscClientWrapper(serialosc_ip, serialosc_port).send_message(
+            "/serialosc/list", "localhost", 15555)
+    pyserialoscutils.OscClientWrapper(
+            device_ip, device_port).send_message("/info", "localhost", 15555)
 
-  while(True):
-    pyserialoscutils.OscClientWrapper(device_ip, device_port).send_message(
-        "/monome/grid/led/set", 5, 7, 1)
-    time.sleep(2)
-    pyserialoscutils.OscClientWrapper(device_ip, device_port).send_message(
-        "/monome/grid/led/set", 5, 7, 0)
-    time.sleep(2)
+    while(True):
+        pyserialoscutils.OscClientWrapper(device_ip, device_port).send_message(
+                "/monome/grid/led/set", 5, 7, 1)
+        time.sleep(2)
+        pyserialoscutils.OscClientWrapper(device_ip, device_port).send_message(
+                "/monome/grid/led/set", 5, 7, 0)
+        time.sleep(2)
--- pyserialoscutils.py
+++ pyserialoscutils.py
@@ -13,30 +13,30 @@ import os
 # chose an implementation, depending on os
 #~ if sys.platform == 'cli':
 #~ else:
-if os.name == 'nt':  # sys.platform == 'win32':
-    from serial.tools.list_ports_windows import comports
+if os.name == 'nt':    # sys.platform == 'win32':
+        from serial.tools.list_ports_windows import comports
 elif os.name == 'posix':
-    from serial.tools.list_ports_posix import comports
+        from serial.tools.list_ports_posix import comports
 else:
-    raise ImportError(
-        "Sorry: no implementation for your platform ('%s') available", os.name)
+        raise ImportError(
+                "Sorry: no implementation for your platform ('%s') available", os.name)
 
 
 # -----------
 # To make it a bit easier to send osc messages
 # -----------
 class OscClientWrapper:
-  def __init__(self, targethost, targetport):
-    super().__init__()
-    self.targethost = map_localhost_to_ip4(targethost)
-    self.targetport = targetport
-    self.__client = SimpleUDPClient(
-        self.targethost, self.targetport)  # Create client
+    def __init__(self, targethost, targetport):
+        super().__init__()
+        self.targethost = map_localhost_to_ip4(targethost)
+        self.targetport = targetport
+        self.__client = SimpleUDPClient(
+                self.targethost, self.targetport)    # Create client
 
-  def send_message(self, address, *osc_arguments):
-    logging.debug("Sending message to %s:%s with path %s and data %s",
-                  self.targethost, self.targetport, address, osc_arguments)
-    self.__client.send_message(address, osc_arguments)
+    def send_message(self, address, *osc_arguments):
+        logging.debug("Sending message to %s:%s with path %s and data %s",
+                                    self.targethost, self.targetport, address, osc_arguments)
+        self.__client.send_message(address, osc_arguments)
 
 # -----------
 # For easy starting and stopping of oscservers
@@ -44,91 +44,91 @@ class OscClientWrapper:
 
 
 class OscServerWrapper:
-  def __init__(self, friendlyname):
-    super().__init__()
-    self.friendlyname = friendlyname
-    self.dispatcher = dispatcher.Dispatcher()
-    self.dispatcher.set_default_handler(self.default_osc_handler, self)
-    self.running = False
+    def __init__(self, friendlyname):
+        super().__init__()
+        self.friendlyname = friendlyname
+        self.dispatcher = dispatcher.Dispatcher()
+        self.dispatcher.set_default_handler(self.default_osc_handler, self)
+        self.running = False
 
-  def default_osc_handler(self, source, *osc_arguments):
-    logging.warn("WARNING: Unhandled OSC message received by %s. Source: %s, Content %s",
-                 self.friendlyname, source, osc_arguments)
+    def default_osc_handler(self, source, *osc_arguments):
+        logging.warn("WARNING: Unhandled OSC message received by %s. Source: %s, Content %s",
+                                 self.friendlyname, source, osc_arguments)
 
-  def start(self, host, port):
-    self.host = map_localhost_to_ip4(host)
-    self.port = port
+    def start(self, host, port):
+        self.host = map_localhost_to_ip4(host)
+        self.port = port
 
-    try:
-      self.__server = osc_server.BlockingOSCUDPServer(
-          (self.host, self.port), self.dispatcher)
-    except OSError as e:
-      logging.warn("WARNING: Error starting OSCUDPServer %s: %s.",
-                   self.friendlyname, e)
-      return False
+        try:
+            self.__server = osc_server.BlockingOSCUDPServer(
+                    (self.host, self.port), self.dispatcher)
+        except OSError as e:
+            logging.warn("WARNING: Error starting OSCUDPServer %s: %s.",
+                                     self.friendlyname, e)
+            return False
 
-    logging.info("Starting OSC server %s on: %s:%s",
-                 self.friendlyname, host, port)
-    self.__server_thread = Thread(target=self.__server.serve_forever)
-    self.__server_thread.start()
-    self.running = True
-    return True
+        logging.info("Starting OSC server %s on: %s:%s",
+                                 self.friendlyname, host, port)
+        self.__server_thread = Thread(target=self.__server.serve_forever)
+        self.__server_thread.start()
+        self.running = True
+        return True
 
-  def stop(self):
-    logging.info("Stopping OSC server: %s", self.friendlyname)
-    if (self.running):
-      self.__server.shutdown()
-      self.__server_thread.join()
+    def stop(self):
+        logging.info("Stopping OSC server: %s", self.friendlyname)
+        if (self.running):
+            self.__server.shutdown()
+            self.__server_thread.join()
 
 
 def map_localhost_to_ip4(host):
-    if (host == "localhost"):
-        return "127.0.0.1"
-    else:
-        return host
+        if (host == "localhost"):
+                return "127.0.0.1"
+        else:
+                return host
 
 
 def find_free_port():
-    with closing(socket.socket(socket.AF_INET, socket.SOCK_DGRAM)) as s:
-        s.bind(('', 0))
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        return s.getsockname()[1]
+        with closing(socket.socket(socket.AF_INET, socket.SOCK_DGRAM)) as s:
+                s.bind(('', 0))
+                s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                return s.getsockname()[1]
 
 
 def list_serial_ports():
-    portlist = []
-    for port in sorted(comports(include_links=True)):
-        portlist.append(port.device)
+        portlist = []
+        for port in sorted(comports(include_links=True)):
+                portlist.append(port.device)
 
-    return portlist
+        return portlist
 
 # unused so far - might include in pyserialoscdevice later
 
 
 def rotate_coordinates(x, y, xsize, ysize, degrees):
-  if (degrees == 0):
-    return (x, y)
-  elif(degrees == 90):
-    return (y, xsize - x - 1)
-  elif(degrees == 180):
-    return (xsize - x - 1, ysize - y - 1)
-  elif(degrees == 270):
-    return (ysize - y - 1, x)
-  else:
-    logging.error(
-        "Only rotations in increments of 90 are allowed (i.e. 0, 90, 180, 270). Got %s. Ignoring rotation.", degrees)
-    return (x, y)
+    if (degrees == 0):
+        return (x, y)
+    elif(degrees == 90):
+        return (y, xsize - x - 1)
+    elif(degrees == 180):
+        return (xsize - x - 1, ysize - y - 1)
+    elif(degrees == 270):
+        return (ysize - y - 1, x)
+    else:
+        logging.error(
+                "Only rotations in increments of 90 are allowed (i.e. 0, 90, 180, 270). Got %s. Ignoring rotation.", degrees)
+        return (x, y)
 
 
 def rotate_map(map, degrees):
-  returnmap = map
-  if (not degrees % 90 == 0):
-    logging.error(
-        "Only rotations in increments of 90 are allowed (i.e. 0, 90, 180, 270). Got %s. Ignoring rotation.", degrees)
-    return
+    returnmap = map
+    if (not degrees % 90 == 0):
+        logging.error(
+                "Only rotations in increments of 90 are allowed (i.e. 0, 90, 180, 270). Got %s. Ignoring rotation.", degrees)
+        return
 
-  rotatetimes = int(degrees / 90)
-  for _ in range(rotatetimes):
-    returnmap = zip(*returnmap[::-1])
+    rotatetimes = int(degrees / 90)
+    for _ in range(rotatetimes):
+        returnmap = zip(*returnmap[::-1])
 
-  return returnmap
+    return returnmap
